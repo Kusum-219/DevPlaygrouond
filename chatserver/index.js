cbc569cf9cb38cb2e597b1f241bf8fda790741ff
vineet dixit <vineetdixit.vns@gmail.com>
2024-10-18 23:51:16 -0700

    Fixed: Socket Cors

--- chatserver/index.js
+++ chatserver/index.js
@@ -5,19 +5,30 @@ const { Server } = require('socket.io');
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*", 
+    methods: ["GET", "POST"]
+  }
+});
 
 app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'index.html'));
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  console.log('a user connected with id:', socket.id);
+  
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
 
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
 });
+
+
 
 // const express = require("express");
 // const http = require("http"); // Import http module
